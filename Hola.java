fc36962f2866f117f1d35b0bc47fb80a6dbb94c2
Carlos Alberto Hernandez Garcia <carlos.hernandez.sc@itszapopan.edu.mx>
2018-08-27 18:23:47 -0500

    Ya se cambio de nuevo XD

--- Hola.java
+++ Hola.java
@@ -4,7 +4,7 @@ public class Hello
           public static void main (String args[])
 	 	{
 			
-			System.out.println("Ke tranza Karnaza Mundo");
+			System.out.println("queovo Karnaza y el Mundo");
 
 		}
 
